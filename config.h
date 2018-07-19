5c8eeb5c1bcd5373cdc2e96d49f7bfb9efc129e3
Mike Bell <hello@mikebell.io>
2018-07-19 10:06:31 +0100

    Update keymap to newest version

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
+Copyright 2017 Danny Nguyen <danny@keeb.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Use I2C or Serial, not both */
 
-//#define USE_SERIAL
-#define USE_I2C
+#define USE_SERIAL
+// #define USE_I2C
 
 /* Select hand configuration */
 
